a1be8cd2166798558bcd47ac93ec3baefd376aaf
takecchi <takecchi.kobayashi@gmail.com>
2023-12-23 16:45:34 +0900

    Storybookのドキュメント整理

--- src/app/_components/Checkbox.stories.ts
+++ src/app/_components/Checkbox.stories.ts
@@ -2,15 +2,15 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Checkbox from './Checkbox';
 
 const meta: Meta<typeof Checkbox> = {
+  title: 'Components/Checkbox',
   component: Checkbox,
   tags: ['autodocs'],
-};
+} satisfies Meta<typeof Checkbox>;
 
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Example: Story = {
   args: {
     label: 'Label',
   },
