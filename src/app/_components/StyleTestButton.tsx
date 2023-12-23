dd3bafed64212048a3c547e97d1e713f59b1428d
takecchi <takecchi.kobayashi@gmail.com>
2023-12-23 16:45:13 +0900

    不要なコンポーネントの削除

--- src/app/_components/StyleTestButton.tsx
+++
@@ -1,9 +0,0 @@
-import { css, styled } from '@kuma-ui/core';
-
-const StyledButton = styled.button`
-  color: red;
-`;
-
-export default function StyleTestButton() {
-  return <StyledButton>TEST</StyledButton>;
-}
